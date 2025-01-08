8f946705185cf99420f435e7e9d7d168f918efa1
lab-arch <1060927911@qq.com>
2025-01-08 12:31:28 +0800

    wrapper posix mutex, spinlock, rwlock; implement LockGuard

--- tests/test_thread.cpp
+++ tests/test_thread.cpp
@@ -1,29 +1,78 @@
 #include "thread.h"
 
+#include <cstddef>
 #include <format>
-#include <memory>
-#include <spdlog/spdlog.h>
-#include <vector>
 #include <iostream>
+#include <memory>
+#include <vector>
+
+#include <spdlog/spdlog.h>
+
+#include "mutex.h"
 
 int main() {
     spdlog::set_level(spdlog::level::debug);
 
     std::vector<std::shared_ptr<saylar::Thread>> threads;
 
-    auto func = []() {
-        auto* current_thread = saylar::Thread::getCurrentThread();
-        std::cout << std::format("name: {}, tid: {}\n", current_thread->getName(), current_thread->getTid());
-        while (true) {}
-    };
+    {
+        std::size_t counter = 0;
+        sylar::Mutex mutex;
 
-    for (size_t i = 0; i < 5; i++) {
-        auto thread = std::make_shared<saylar::Thread>(func, std::format("thread_{}", i));
-        thread->start();
-        threads.push_back(thread);
+        auto worker = [&]() {
+            while (true) {
+                sylar::LockGuard lock(mutex);
+                counter++;
+            }
+        };
+
+        for (size_t i = 0; i < 5; i++) {
+            auto thread = std::make_shared<saylar::Thread>(worker, std::format("thread_{}", i));
+            thread->start();
+            threads.push_back(std::move(thread));
+        }
+        for (auto& thread : threads) {
+            thread->join();
+        }
+
+        std::cout << counter << '\n';
     }
 
-    for (auto & thread: threads) {
-        thread->join();
-    }
+    // {
+    //     std::size_t counter = 0;
+    //     sylar::RWMutex mutex;
+    //
+    //     auto writer = [&]() {
+    //         while (true) {
+    //             sylar::WriteLockGuard lock(mutex);
+    //             counter ++;
+    //         }
+    //     };
+    //
+    //     auto reader = [&]() {
+    //         std::size_t val;
+    //         while(true) {
+    //             {
+    //                 sylar::ReadLockGuard lock(mutex);
+    //                 val = counter;
+    //             }
+    //         }
+    //         (void) val;
+    //     };
+    //
+    //     for (size_t i = 0; i < 5; i++) {
+    //         auto thread = std::make_shared<saylar::Thread>(reader, std::format("thread_read_{}", i));
+    //         thread->start();
+    //         threads.push_back(std::move(thread));
+    //     }
+    //     for (size_t i = 0; i < 5; i++) {
+    //         auto thread = std::make_shared<saylar::Thread>(writer, std::format("thread_write_{}", i));
+    //         thread->start();
+    //         threads.push_back(std::move(thread));
+    //     }
+    //
+    //     for (auto& thread : threads) {
+    //         thread->join();
+    //     }
+    // }
 }
