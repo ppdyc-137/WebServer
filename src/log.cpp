515d18ea56a0117254b562946e929a61325fb48f
ppdy <1060927911@qq.com>
2025-02-27 16:18:44 +0800

    add log initer; add TheadFiberId pattern for logging

---
+++ src/log.cpp
@@ -0,0 +1,39 @@
+#include "log.h"
+#include "fiber.h"
+#include "thread.h"
+
+#include <spdlog/common.h>
+#include <spdlog/spdlog.h>
+
+#include "spdlog/pattern_formatter.h"
+
+class ThreadFiberIdFlag : public spdlog::custom_flag_formatter {
+public:
+    void format(const spdlog::details::log_msg& /*msg*/, const std::tm& /*tm_time*/,
+                spdlog::memory_buf_t& dest) override {
+        std::string text{};
+        if (auto* thread = sylar::Thread::getCurrentThread(); thread != nullptr) {
+            text += thread->getName() + " ";
+        }
+        text += std::format("{} {}", sylar::Thread::getCurrentThreadId(), sylar::Fiber::getCurrentFiberId());
+        dest.append(text.data(), text.data() + text.size());
+    }
+
+    std::unique_ptr<custom_flag_formatter> clone() const override {
+        return spdlog::details::make_unique<ThreadFiberIdFlag>();
+    }
+};
+
+namespace sylar {
+
+    namespace {
+        struct LogIniter {
+            LogIniter() {
+                auto formatter = std::make_unique<spdlog::pattern_formatter>();
+                formatter->add_flag<ThreadFiberIdFlag>('*').set_pattern("[%Y-%m-%d %H:%M:%S:%e] [%*] [%^%l%$] %v");
+                spdlog::set_formatter(std::move(formatter));
+            }
+        } g_log_initer;
+    } // namespace
+
+} // namespace sylar
--- tests/test_fiber.cpp
+++ tests/test_fiber.cpp
@@ -4,20 +4,6 @@
 #include <spdlog/common.h>
 #include <spdlog/spdlog.h>
 
-#include "spdlog/pattern_formatter.h"
-
-class my_formatter_flag : public spdlog::custom_flag_formatter {
-public:
-    void format(const spdlog::details::log_msg&, const std::tm&, spdlog::memory_buf_t& dest) override {
-        std::string some_txt = std::format("{} {}", sylar::Thread::getCurrentThreadId(), sylar::Fiber::getCurrentFiberId());
-        dest.append(some_txt.data(), some_txt.data() + some_txt.size());
-    }
-
-    std::unique_ptr<custom_flag_formatter> clone() const override {
-        return spdlog::details::make_unique<my_formatter_flag>();
-    }
-};
-
 void foo() {
     spdlog::info("hello");
     sylar::Fiber::yield();
@@ -38,12 +24,7 @@ void test() {
 }
 
 int main() {
-    {
-        auto formatter = std::make_unique<spdlog::pattern_formatter>();
-        formatter->add_flag<my_formatter_flag>('*').set_pattern("[%Y-%m-%d %H:%M:%S:%e] [%*] [%^%l%$] %v");
-        spdlog::set_formatter(std::move(formatter));
-        spdlog::set_level(spdlog::level::debug);
-    }
+    spdlog::set_level(spdlog::level::debug);
 
     sylar::Fiber::getCurrentFiber();
 
